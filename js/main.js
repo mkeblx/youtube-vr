a8586abe196bc37c18d0302dd003005589128983
Michael Blix <mike@quasi.co>
2014-07-20 20:26:53 -0400

    screens

--- js/main.js
+++ js/main.js
@@ -6,8 +6,12 @@ var time = Date.now();
 var scene, camera, container, renderer, controls;
 var scene2, camera2, renderer2, controls2;
 
+var element, element2;
+
 init();
 animate();
+
+var crossDomain = 0;
 
 function init() {
 
@@ -17,17 +21,9 @@ function init() {
 
 	camera = new THREE.PerspectiveCamera( 75, 640 / 800, 1, 5000 );
 	camera.position.x = -IPD;
-	camera.position.y = -35;
-	camera.position.z = 0;
-
-	//camera.rotation.z = 0.4;
 
 	camera2 = new THREE.PerspectiveCamera( 75, 640 / 800, 1, 5000 );
 	camera2.position.x = IPD;
-	camera2.position.y = -35;
-	camera2.position.z = 0;
-
-	//camera2.rotation.z = 0.4;
 
 	scene = new THREE.Scene();
 	scene2 = new THREE.Scene();
@@ -48,33 +44,43 @@ function init() {
 	renderer2.domElement.style.top = 0;
 	document.getElementById('viewbox1').appendChild( renderer2.domElement );
 
-	var site = 'http://threejs.org';
+	crossDomain = false;
+	var screens = [{
+		url: 'http://threejs.org',//'http://dev.quasi.co/css3d-3d/embed.html',
+		position: [0, -25, -1100],
+		rotation: [0, 0, 0],
+		size: 1
+	}];
 
-	for ( var i = 0; i < 1; i ++ ) {
-		var element = document.createElement( 'div' );
+	var screen;
+
+	for ( var i = 0; i < screens.length; i++ ) {
+		screen = screens[i];
+
 		element = document.createElement('iframe');
-		element.setAttribute('src', site);
-		element.style.width = '1024px';
-		element.style.height = '800px';
-		//var color = new THREE.Color( Math.random() * 0xffffff ).getStyle();
-		//element.style.background = color;
+		element.setAttribute('src', screens[i].url);
+		//element.setAttribute('seamless', true);
+		element.style.width = '1300px';
+		element.style.height = '900px';
+		var color = new THREE.Color( Math.random() * 0xffffff ).getStyle();
+		element.style.background = color;
 
 		var r0 = Math.random(), r1 = Math.random(), r2 = Math.random(), r3 = Math.random();
 		var r4 = Math.random(), r5 = Math.random(), r6 = Math.random(), r7 = Math.random();
 
 		var object = new THREE.CSS3DObject( element );
-		object.position.set(0, -25, -1100);
-		//object.rotation.set(0,0,0);
-		//object.scale.set(1,1,1);
+		object.position.fromArray(screen.position);
+		object.rotation.fromArray(screen.rotation);
+		//object.scale.fromArray(screen.scale);
 		scene.add( object );
 
 
-		var element2 = element.cloneNode(true);
+		element2 = element.cloneNode(true);
 
 		var object2 = new THREE.CSS3DObject( element2 );
 		object2.position.copy(object.position);
 		object2.rotation.copy(object.rotation);
-		//object2.scale.copy(object.copy);
+		//object2.scale.copy(object.scale);
 		scene2.add( object2 );
 
 
@@ -88,9 +94,35 @@ function init() {
 
 }
 
+var once = true;
+
 function animate() {
 	requestAnimationFrame(animate);
 	var dt = clock.getDelta();
+
+
+	var top;
+	var doc, doc2;
+
+
+	if (!crossDomain) {
+		//doc = element.contentDocument || element.contentWindow.document || null;
+		//doc2 = element2.contentDocument || element2.contentWindow.document || null;
+
+		if (doc) {
+			top = doc.body.parentElement.scrollTop;
+			if (once) {
+				window.doc = doc;
+				console.log(doc);
+				once = false;
+			}
+//			console.log(element.contentWindow.document.body);
+			if (top != 0) { 
+				console.log(top);
+			}
+			element2.contentWindow.scrollTo(0, top);
+		}
+	}
 
 	controls.update();
 	controls2.update();
